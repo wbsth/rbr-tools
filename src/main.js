9784b9dcd507d24cdf3928ac7c32cd10aa075032
Michal <18263943+wbsth@users.noreply.github.com>
2022-11-27 16:25:15 +0100

    zmiana na splitpanes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import GridLayout from 'vue3-drr-grid-layout'
-import 'vue3-drr-grid-layout/dist/style.css'
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 import "./main.css";
+
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
-
 /* import specific icons */
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
-
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
 library.add(faXmark);
 
 const app = createApp(App);
 
 app.use(router);
-app.use(GridLayout);
-app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.component("font-awesome-icon", FontAwesomeIcon)
+   .component("Splitpanes", Splitpanes)
+   .component("Pane", Pane)
 
 app.mount("#app");
--- src/views/TelemetryView.vue
+++ src/views/TelemetryView.vue
@@ -1,68 +1,21 @@
 <script setup>
-    const layout = [
-            { x: 0, y: 0, w: 2, h: 5, i: 0, },
-            { x: 0, y: 0, w: 2, h: 5, i: 1 },
-          ];
+
 </script>
 
 <template>
     <div class="bg-neutral-800 rounded-md p-4">
         <h1 class="font-bold text-3xl">Telemetry analyzer</h1>
 
-        <grid-layout v-model:layout="layout"
-                    :col-num="2"
-                    :row-height="30">
-
-            <template #default="{ gridItemProps }">
-        <!-- | gridItemProps props from GridLayout | -->
-        <!--breakpointCols: props.cols-->
-        <!--colNum: props.colNum-->
-        <!--containerWidth: width.value-->
-        <!--isDraggable: props.isDraggable-->
-        <!--isResizable: props.isResizable-->
-        <!--lastBreakpoint: lastBreakpoint.value-->
-        <!--margin: props.margin-->
-        <!--maxRows: props.maxRows-->
-        <!--responsive: props.responsive-->
-        <!--rowHeight: props.rowHeight-->
-        <!--useCssTransforms: props.useCssTransforms-->
-        <!--width: width.value-->
-            <grid-item 
-                v-for="item in layout"
-                :key="item.i"
-                class="bg-neutral-700"
-                v-bind="gridItemProps"
-                :x="item.x"
-                :y="item.y"
-                :w="item.w"
-                :h="item.h"
-                :i="item.i"
-                drag-allow-from=".drag"
-                drag-ignore-from=".no-drag">
-            <div class="grid grid-cols-2 bg-neutral-900 px-2 py-1 no-drag">
-                <div>
-                    <p>NAME PLACEHOLDER</p>
-                </div>
-
-                <div class="justify-self-end">
-                    <font-awesome-icon icon="fa-solid fa-xmark" />
-                </div>
-            </div>
-        </grid-item>
-      </template>
-
-        </grid-layout>
-        <!-- <grid-layout
-            v-model:layout="layout"
-            :col-num="1"
-            :row-height="30"
-            @resize="resize"
-            @move="move"
-            @moved="moved">
-            
-            <template #item="{ item }">
-                <p>TEATSAT</p>
-            </template>
-        </grid-layout> -->
+        <splitpanes class="default-theme" horizontal style="height: 400px">
+            <pane min-size="20" max-size="70" class="bg-red-300">
+                <span>1</span>
+            </pane>
+            <pane>
+                <span>2</span>
+            </pane>
+            <pane max-size="70">
+                <span>3</span>
+            </pane>
+        </splitpanes>
     </div>
 </template>
