5839e222eae038b612e07a133ab28e7ad03f3c8a
Michal <18263943+wbsth@users.noreply.github.com>
2023-03-20 17:38:07 +0100

    finished homepage

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
 import { inject } from "@vercel/analytics";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "Setup extractor", href: "/extractor" },
+  { name: "Setup replacer", href: "/replacer" },
+  { name: "Telemetry analyzer", href: "/telemetry" },
+];
+
 inject();
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <span class="text-red-600 font-bold text-2xl">RBR </span>
     <span class="text-white font-bold text-2xl">TOOLS</span>
-  </div>
+  </div> -->
 
-  <div class="my-2 bg-neutral-800 rounded-md">
+  <!-- <div class="my-2 bg-neutral-800 rounded-md">
     <router-link
       to="/"
       class="ml-2 py-2 px-3 hover:bg-neutral-700 inline-block">
@@ -35,7 +52,82 @@ inject();
     </router-link>
 
     <p class="py-2 px-3 inline-block">more soon...</p>
-  </div>
+  </div> -->
+
+  <Disclosure v-slot="{ open }" as="nav" class="bg-neutral-800 rounded-md my-2">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex flex-row h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden z-10">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-stone-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            <font-awesome-icon
+              v-if="!open"
+              class="block h-4 w-4"
+              icon="fa-solid fa-bars"
+              aria-hidden="true" />
+
+            <font-awesome-icon
+              v-else
+              class="block h-4 w-4"
+              icon="fa-solid fa-xmark"
+              aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start z-0">
+          <div class="flex flex-shrink-0 items-center">
+            <span class="text-red-600 font-bold text-2xl mr-1">RBR</span>
+            <span class="text-white font-bold text-2xl">TOOLS</span>
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <!-- <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              > -->
+              <router-link
+                v-for="item in navigation"
+                :key="item.name"
+                :to="item.href"
+                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-stone-600 hover:text-white">
+                {{ item.name }}
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel v-slot="{ close }" class="sm:hidden">
+      <div class="space-y-1 px-2 pt-2 pb-3">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          class="block rounded-md px-3 py-2 text-base font-medium">
+          <button @click="close">
+            <router-link
+              :key="item.name"
+              :to="item.href"
+              class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-stone-600 hover:text-white">
+              {{ item.name }}
+            </router-link>
+          </button>
+        </DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
 
   <RouterView v-slot="{ Component }">
     <Transition mode="out-in">
@@ -64,7 +156,7 @@ inject();
 }
 
 .router-link-active {
-  color: white;
-  @apply bg-neutral-700;
+  @apply text-gray-100;
+  @apply bg-stone-700;
 }
 </style>
--- src/components/HomeTile.vue
+++ src/components/HomeTile.vue
@@ -13,19 +13,21 @@ function getImageUrl() {
 <template>
   <div
     class="flex flex-col sm:flex-row bg-neutral-700 rounded-md overflow-hidden border-2 border-neutral-900">
-    <div class="basis-2/3 p-2">
-      <router-link :to="props.tileData.tileUrlPath">
+    <div class="basis-2/3 p-4">
+      <a :href="props.tileData.tileUrlPath">
         <h1 class="font-bold text-lg mb-2 hover:text-neutral-400">
           {{ props.tileData.tileTitle }}
         </h1>
-      </router-link>
+      </a>
 
       <p class="text-justify mx-2">
         {{ props.tileData.tileDescription }}
       </p>
     </div>
     <div class="basis-1/3">
-      <img class="object-cover w-full h-full" :src="getImageUrl()" />
+      <a :href="props.tileData.tileUrlPath">
+        <img class="object-cover w-full h-full" :src="getImageUrl()" />
+      </a>
     </div>
   </div>
 </template>
